31a7c3e6fe4e5dec88d52583c92f49fe9020964d
Jiyoung Ko <jclareko@gmail.com>
2020-01-29 17:18:27 +0900

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'chatroom#index'
+  get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
