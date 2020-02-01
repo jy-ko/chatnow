b5bf6b73878964fb727e0f47e72e821cdbfcf549
Jiyoung Ko <jclareko@gmail.com>
2020-02-01 13:22:40 +0900

    message field fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Abdallah", password: "password")
+User.create(username: "Jiyoung", password: "password")
